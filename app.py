471bf3f6cbdb3062c74dd55d07edec2e7d8afb1f
unknown <sanjeevkr4983@gmail.com>
2022-01-06 12:28:44 +0530

    testing changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
-    return "Flask app is running"
+    return {"HI":"HOw aer you"}
   
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=5000)
