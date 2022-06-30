3a884ccdc587c904bf73f2885a72016a33b3bd0b
sanjeev kumar <sanjeevkr4983@gmail.com>
2022-06-30 16:36:31 +0530

    spelling correction

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
-    return {"HI":"HOw aer you"}
+    return {"HI":"How aer you"}
   
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=5000)
